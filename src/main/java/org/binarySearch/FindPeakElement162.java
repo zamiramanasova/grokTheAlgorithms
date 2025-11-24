33408edd6b0c4e5651e782ee9b1a42632b8eb360
Zamira <keldibaevaz@gmail.com>
2025-11-24 22:48:31 +0300

    task four

--- src/main/java/org/binarySearch/FindPeakElement162.java
+++ src/main/java/org/binarySearch/FindPeakElement162.java
@@ -2,6 +2,23 @@ package org.binarySearch;
 
 public class FindPeakElement162 {
     public static void main(String[] args) {
+        int[] array = {1,2,3,1};
+        int result = findPeakElement(array);
+        System.out.println(result);
+    }
+    public static int findPeakElement(int[] nums) {
+       int low = 0;
+       int high = nums.length - 1;
 
+       while(low < high) {
+           int mid = (high + low) / 2;
+
+           if (nums[mid] > nums[mid + 1]) {
+               high = mid;
+           } else {
+               low = mid + 1;
+           }
+       }
+       return low;
     }
 }
