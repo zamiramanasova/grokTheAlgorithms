4cbf1a04446cb9149ffd4247ab926df584d0b491
Zamira <keldibaevaz@gmail.com>
2025-11-26 22:52:47 +0300

    task five

--- src/main/java/org/simpleTasks/LengthOfTheLastWord.java
+++ src/main/java/org/simpleTasks/LengthOfTheLastWord.java
@@ -15,7 +15,7 @@ package org.simpleTasks;
  */
 public class LengthOfTheLastWord {
     public static void main(String[] args) {
-       String s = "Hello World";
+        String s = "Hello World";
         System.out.println(s);
     }
 
