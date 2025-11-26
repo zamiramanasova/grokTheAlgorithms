7bcf76b2c379ea2e4c7859a39453b1fcaee9a217
Zamira <keldibaevaz@gmail.com>
2025-11-26 22:37:31 +0300

    task five

--- src/main/java/org/simpleTasks/LengthOfTheLastWord.java
+++ src/main/java/org/simpleTasks/LengthOfTheLastWord.java
@@ -1,7 +1,29 @@
 package org.simpleTasks;
 
+/**
+ * Имея строку, состоящую из слов и пробелов,
+ * возвращаем длину последнего слова в строке.s
+ *
+ * Слово — это максимум подстрока состоящий
+ * только из символов, не размещающихся в пробелах.
+ *
+ * Пример 1:
+ *
+ * Input: s = "Hello World"
+ * Output: 5
+ * Explanation: The last word is "World" with length 5
+ */
 public class LengthOfTheLastWord {
     public static void main(String[] args) {
+       String s = "Hello World";
+        char[] array = s.toCharArray();
+        System.out.println();
+    }
 
+    public int lengthOfLastWord(String s) {
+       char[] array = s.toCharArray();
+       for (int i = 0; i < array.length - 1; i++) {
+           if ()
+       }
     }
 }
