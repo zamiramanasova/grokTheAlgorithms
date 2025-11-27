d4a0bc6f426051c97ffa5233480401249017bc7d
Zamira <keldibaevaz@gmail.com>
2025-11-27 23:31:18 +0300

    1

--- src/main/java/org/binarySearch/FindTargetIndicesAfterSortingArray2089.java
+++ src/main/java/org/binarySearch/FindTargetIndicesAfterSortingArray2089.java
@@ -1,6 +1,8 @@
 package org.binarySearch;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class FindTargetIndicesAfterSortingArray2089 {
@@ -8,18 +10,23 @@ public class FindTargetIndicesAfterSortingArray2089 {
 
     }
 
-    public List<Integer> targetIndices(int[] nums, int target) {
+    public static List<Integer> targetIndices(int[] nums, int target) {
         //дается массив
         //отсортировать массив
         //вывести индексы расположения таргета
         Arrays.sort(nums);
-        int low = 0;
-        int high = nums.length - 1;
-        int mid = 0;
 
-        while (low <= high) {
-            mid = (low + high) / 2;
+        List<Integer> array = new ArrayList<>();
 
+        for (int i = 0; i < nums.length; i++) {
+                array.add(i);
         }
+
+        for (Integer index : array) {
+            if (index == target)
+                return Collections.singletonList(index);
+        }
+
+        return
     }
 }
