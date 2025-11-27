9e09487b7f6f2ff69ac830a404cf4086ad5e0b4a
Zamira <keldibaevaz@gmail.com>
2025-11-27 23:09:42 +0300

    1

--- src/main/java/org/binarySearch/FindTargetIndicesAfterSortingArray2089.java
+++ src/main/java/org/binarySearch/FindTargetIndicesAfterSortingArray2089.java
@@ -1,4 +1,25 @@
 package org.binarySearch;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FindTargetIndicesAfterSortingArray2089 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> targetIndices(int[] nums, int target) {
+        //дается массив
+        //отсортировать массив
+        //вывести индексы расположения таргета
+        Arrays.sort(nums);
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+        }
+    }
 }
