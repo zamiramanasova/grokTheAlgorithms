8f28a8a60572e668dd7125c90c54056274a62383
Zamira <keldibaevaz@gmail.com>
2025-11-29 13:46:37 +0300

    part 1

---
+++ src/main/java/org/binarySearch/PeakIndexinaMountainArray852.java
@@ -0,0 +1,31 @@
+package org.binarySearch;
+
+/**
+ * Вам даётся целочисленный горный массив длины,
+ * значения увеличиваются до пика, а затем уменьшаются.arrn
+ *
+ * Верните индекс пикового элемента.
+ *
+ * Ваша задача — решить её с ограниченными временами.O(log(n))
+ *
+ * Пример 1:
+ *
+ * Вход: arr = [0,1,0]
+ *
+ * Выпуск: 1
+ *
+ * Пример 2:
+ *
+ * Вход: arr = [0,2,1,0]
+ *
+ * Выпуск: 1
+ */
+public class PeakIndexinaMountainArray852 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int peakIndexInMountainArray(int[] array) {
+
+    }
+}
