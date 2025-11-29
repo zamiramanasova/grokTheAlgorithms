874b6e22fe1ffcce5864612f4aae44490a0af4a9
Zamira <keldibaevaz@gmail.com>
2025-11-29 14:27:50 +0300

    part 1

--- src/main/java/org/binarySearch/PeakIndexinaMountainArray852.java
+++ src/main/java/org/binarySearch/PeakIndexinaMountainArray852.java
@@ -22,10 +22,23 @@ package org.binarySearch;
  */
 public class PeakIndexinaMountainArray852 {
     public static void main(String[] args) {
-
+        int[] arr = new int[] {1,3,0};
+        System.out.println(peakIndexInMountainArray(arr));
     }
 
     public static int peakIndexInMountainArray(int[] array) {
+        int left = 0;// это обозначение индекса
+        int right = array.length - 1; // обозначение индекса
 
+        while (left <= right) {
+            int middle = left + (right - left) / 2; //вычисление ИНДЕКСА, не элемента.
+
+            if (array[middle] <= array[middle + 1]) { // если элемент middle меньше элемента middle+1, то
+                left = middle + 1; // ищем справа
+            } else {
+                right = middle - 1; // ищем слева
+            }
+        }
+        return left;
     }
 }
