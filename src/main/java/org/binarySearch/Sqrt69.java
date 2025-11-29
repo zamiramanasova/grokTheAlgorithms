0e0c38fbc40fd9a078a8d830689b5e6b6c9bcf7b
Zamira <keldibaevaz@gmail.com>
2025-11-29 11:06:12 +0300

    part 1

--- src/main/java/org/binarySearch/Sqrt69.java
+++ src/main/java/org/binarySearch/Sqrt69.java
@@ -9,5 +9,21 @@ public class Sqrt69 {
         if (x < 2) {
             return x;
         }
+
+        int left = 1;
+        int right = x / 2;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x/mid) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return answer;
     }
 }
