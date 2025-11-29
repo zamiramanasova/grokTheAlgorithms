c58eb2fbf35a5512acb0efa4b3eac81c685545ab
Zamira <keldibaevaz@gmail.com>
2025-11-29 11:02:36 +0300

    part 1

---
+++ src/main/java/org/binarySearch/Sqrt69.java
@@ -0,0 +1,13 @@
+package org.binarySearch;
+
+public class Sqrt69 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+    }
+}
