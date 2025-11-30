12920350bedac18e3a3ec8837a5c1b6761f62682
Zamira <keldibaevaz@gmail.com>
2025-11-30 19:15:32 +0300

    sorting

--- src/main/java/org/sorting/SortingMinimalElements.java
+++ src/main/java/org/sorting/SortingMinimalElements.java
@@ -1,12 +1,16 @@
 package org.sorting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Задача делает поиск минимального элемента и выводит его индекс
  */
 public class SortingMinimalElements {
     public static void main(String[] args) {
         int[] array = new int[] {4,1,3};
-        System.out.println(sortingMinimalElements(array));
+        System.out.println(Arrays.toString(selectSort(array)));
     }
 
     public static int sortingMinimalElements(int[] nums) {
@@ -20,5 +24,17 @@ public class SortingMinimalElements {
             }
         }
         return smallest_index;
+    }
+
+    public static List[] selectSort(int[] array) {
+        //int[] newArray = new int[array.length];
+        List<Integer> newArray = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            int smallest = sortingMinimalElements(array);
+            newArray.add(smallest);
+
+        }
+        return new List[]{newArray};
     }
 }
