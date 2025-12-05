6e81c42318034692c73c160e89a667e301c1059b
Zamira <keldibaevaz@gmail.com>
2025-12-05 20:38:43 +0300

    arrays

---
+++ src/main/java/org/simpleTasks/ValidAnagram242.java
@@ -0,0 +1,27 @@
+package org.simpleTasks;
+
+/**
+ * Given two strings and , return if is an st true tanagram of ,
+ * and otherwise.s false
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ *
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ *
+ * Output: false
+ */
+public class ValidAnagram242 {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean anagram(String s, String t) {
+
+    }
+
+}
