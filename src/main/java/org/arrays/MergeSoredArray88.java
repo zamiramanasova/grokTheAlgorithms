870f03e0dff2edeaee487b14d0eeda27ecc1be4b
Zamira <keldibaevaz@gmail.com>
2025-12-05 20:52:53 +0300

    arrays

---
+++ src/main/java/org/arrays/MergeSoredArray88.java
@@ -0,0 +1,14 @@
+package org.arrays;
+
+public class MergeSoredArray88 {
+    public static void main(String[] args) {
+        int[] nums1 = new int[] {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = new int[] {2,5,6};
+        int n = 3;
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+}
