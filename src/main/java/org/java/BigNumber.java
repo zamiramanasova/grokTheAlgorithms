563ae845e9665d190ce0e4c0ac406ba335bd55c7
Zamira <keldibaevaz@gmail.com>
2025-12-10 20:26:05 +0300

    arrays

---
+++ src/main/java/org/java/BigNumber.java
@@ -0,0 +1,11 @@
+package org.java;
+
+public class BigNumber {
+    public static void main(String[] args) {
+        int a = 1234;
+        int b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a * b);
+    }
+}
