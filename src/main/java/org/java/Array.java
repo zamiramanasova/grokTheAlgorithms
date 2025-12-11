ce45fcbd4489e83106e93eabadbcc3a555f3defd
Zamira <keldibaevaz@gmail.com>
2025-12-11 20:26:04 +0300

    arrays

---
+++ src/main/java/org/java/Array.java
@@ -0,0 +1,19 @@
+package org.java;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int[] a = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
