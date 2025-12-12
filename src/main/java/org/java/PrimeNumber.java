fcf07b5acb8a5b8b5aa3566ce912aab00179277b
Zamira <keldibaevaz@gmail.com>
2025-12-12 22:47:04 +0300

    arrays

---
+++ src/main/java/org/java/PrimeNumber.java
@@ -0,0 +1,21 @@
+package org.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class PrimeNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger possiblePrime = new BigInteger(n);
+
+
+        bufferedReader.close();
+    }
+}
+    BigInteger possiblePrime = new BigInteger("12345678901234567890").
+    boolean isPrime = possiblePrime.isProbablePrime(10).
+            System.out.println("Простое ли число: " + isPrime).
