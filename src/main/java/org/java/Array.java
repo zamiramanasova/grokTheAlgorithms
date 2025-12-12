788534b3e8b8c12517c60d79b9592b6a3c3015f0
Zamira <keldibaevaz@gmail.com>
2025-12-12 22:35:57 +0300

    arrays

--- src/main/java/org/java/Array.java
+++ src/main/java/org/java/Array.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int[] a = new int[number];
