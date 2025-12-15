69762cb26a9bbcd4f96f8bb52e188e0e79ae15a7
Zamira <keldibaevaz@gmail.com>
2025-12-15 15:37:37 +0300

    arrays

---
+++ src/main/java/org/java/ReadInput.java
@@ -0,0 +1,20 @@
+package org.java;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadInput {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(String.valueOf(+ 1));
+        int lineNumber = 1;
+        while (scanner.hasNextLine()) {
+            System.out.println(lineNumber + " " + scanner.nextLine());
+            lineNumber++;
+        }
+
+        scanner.close();
+
+    }
+}
