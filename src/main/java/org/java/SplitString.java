0c667ee0b62987fb323d465dceb4a2190148ce4d
Zamira <keldibaevaz@gmail.com>
2025-12-16 21:19:57 +0300

    arrays

--- src/main/java/org/java/SplitString.java
+++ src/main/java/org/java/SplitString.java
@@ -7,8 +7,10 @@ public class SplitString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = Arrays.toString(sc.nextLine().split(" "));
+        for(String newWord : word.split(" ")){
+            System.out.println(newWord);
+        }
 
-        System.out.println(word);
         sc.close();
     }
 }
