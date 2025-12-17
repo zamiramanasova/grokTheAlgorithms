118d8ee7b3e0f68b1bdae459f081dfd97d225b9f
Zamira <keldibaevaz@gmail.com>
2025-12-17 22:37:42 +0300

    arrays

--- src/main/java/org/java/ArrayListLesson.java
+++ src/main/java/org/java/ArrayListLesson.java
@@ -1,7 +1,16 @@
 package org.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayListLesson {
     public static void main(String[] args) {
-
+        ArrayList list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Введите число: ");
+            list.add(scanner.nextInt());
+        }
+        scanner.close();
     }
 }
