3345b54c95f9021cb0b835607a83c36b0ae22720
Zamira <keldibaevaz@gmail.com>
2025-12-19 20:51:43 +0300

    arrays

--- src/main/java/org/java/StringSubstring.java
+++ src/main/java/org/java/StringSubstring.java
@@ -1,8 +1,13 @@
 package org.java;
 
+import java.util.Scanner;
+
 public class StringSubstring {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int k = sc.nextInt();
+        System.out.println(getSmallestAndLargest(s,k));
     }
 
     public static String getSmallestAndLargest(String s, int k) {
