049858dd998b27fc56411442466b3993b12f127e
Zamira <keldibaevaz@gmail.com>
2025-12-29 22:20:07 +0300

    arrays

--- src/main/java/org/lessonWithTeacher/FindSumArray.java
+++ src/main/java/org/lessonWithTeacher/FindSumArray.java
@@ -6,7 +6,7 @@ public class FindSumArray {
         int sum = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            sum = nums[i] + nums[i];
+            sum = sum + nums[i];
         }
         return sum;
     }
