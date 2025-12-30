22dfa82c537401c0b87e820a01819fc93d364f75
Zamira <keldibaevaz@gmail.com>
2025-12-30 13:36:15 +0300

    arrays

--- src/main/java/org/lessonWithTeacher/CountEvenInArray.java
+++ src/main/java/org/lessonWithTeacher/CountEvenInArray.java
@@ -1,4 +1,19 @@
 package org.lessonWithTeacher;
 
 public class CountEvenInArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,0};
+        System.out.println(countEven(array));
+    }
+
+    public static int countEven(int[] array) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
