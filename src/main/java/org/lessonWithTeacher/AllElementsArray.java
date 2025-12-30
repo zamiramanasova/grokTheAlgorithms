3e6a92b64b7870be849b2bc6a04e8acbfade739b
Zamira <keldibaevaz@gmail.com>
2025-12-30 12:57:45 +0300

    arrays

---
+++ src/main/java/org/lessonWithTeacher/AllElementsArray.java
@@ -0,0 +1,10 @@
+package org.lessonWithTeacher;
+
+public class AllElementsArray {
+    public static void main(String[] args) {
+        int array[] = new int[] {1,2,3,4};
+        for (int i = 0 ; i < array.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
