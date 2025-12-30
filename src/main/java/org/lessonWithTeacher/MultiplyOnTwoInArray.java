44ff6b611a1d631d947e591a9a10eea9eb4e1009
Zamira <keldibaevaz@gmail.com>
2025-12-30 13:54:24 +0300

    arrays

---
+++ src/main/java/org/lessonWithTeacher/MultiplyOnTwoInArray.java
@@ -0,0 +1,19 @@
+package org.lessonWithTeacher;
+
+import java.util.Arrays;
+
+public class MultiplyOnTwoInArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,0};
+        System.out.println(Arrays.toString(multiplyOnTwo(array)));
+    }
+
+    public static int[] multiplyOnTwo(int[] array) {
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i] * 2;
+        }
+        return newArray;
+    }
+}
