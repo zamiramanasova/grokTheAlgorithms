f765d8c5ad8834228c07017db4db18ea494a1dbb
Zamira <keldibaevaz@gmail.com>
2025-12-30 14:12:29 +0300

    arrays

---
+++ src/main/java/org/lessonWithTeacher/CopyOfArray.java
@@ -0,0 +1,19 @@
+package org.lessonWithTeacher;
+
+import java.util.Arrays;
+
+public class CopyOfArray {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,0};
+        System.out.println(Arrays.toString(copyOfArray(array)));
+    }
+
+    public static int[] copyOfArray(int[] array) {
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+           newArray[i] = array[i];
+        }
+        return newArray;
+    }
+}
