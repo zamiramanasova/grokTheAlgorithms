be753405168799d920102a565373694ff52f66cd
Zamira <keldibaevaz@gmail.com>
2026-01-04 21:29:07 +0300

    arrays

--- src/main/java/org/lessonWithTeacher/AllElementsArray.java
+++ src/main/java/org/lessonWithTeacher/AllElementsArray.java
@@ -2,6 +2,7 @@ package org.lessonWithTeacher;
 
 public class AllElementsArray {
     public static void main(String[] args) {
+
         int array[] = new int[] {1,2,3,4};
         for (int i = 0 ; i < array.length; i++) {
             System.out.println(array[i]);
