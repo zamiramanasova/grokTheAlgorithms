1aec9748461f7c02d2a7cd449ae5ed99815e2c06
Zamira <keldibaevaz@gmail.com>
2026-01-05 14:03:26 +0300

    repeat

--- src/main/java/org/lessonWithTeacher/RepeatAfterLesson.java
+++ src/main/java/org/lessonWithTeacher/RepeatAfterLesson.java
@@ -4,6 +4,8 @@ public class RepeatAfterLesson {
     public static void main(String[] args) {
         int[] array1 = new int[] {1,2,3,4,5};
         allElementsInArray(array1);
+        System.out.println("----------------------");
+        System.out.println(sumOffElements(array1));
     }
 
     //вывести все элементы массива
@@ -13,5 +15,14 @@ public class RepeatAfterLesson {
             System.out.println(array1[i]);
         }
     }
+    //сумму всех элементов
+    public static int sumOffElements(int[] array) {
+        int sum = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+
+        return sum;
+    }
 }
