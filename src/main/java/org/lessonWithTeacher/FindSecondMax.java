8afa2e113bafd740e731563e84d3391b756eff25
Zamira <keldibaevaz@gmail.com>
2026-01-05 17:14:08 +0300

    repeat

---
+++ src/main/java/org/lessonWithTeacher/FindSecondMax.java
@@ -0,0 +1,24 @@
+package org.lessonWithTeacher;
+
+public class FindSecondMax {
+    public static void main(String[] args) {
+        int[] array1 = new int[] {1,2,5,4,7};
+        System.out.println(secondMax(array1));
+    }
+
+    public static int secondMax(int[] array) {
+        int maxIndex = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxIndex) {
+                secondMax = maxIndex;
+                maxIndex = array[i];
+            } else if (array[i] > secondMax && array[i] != maxIndex) {
+                secondMax = array[i];
+            }
+
+        }
+        return secondMax;
+    }
+}
