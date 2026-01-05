5609726f8b0f28d6df2d5fd6311c82c356444133
Zamira <keldibaevaz@gmail.com>
2026-01-05 13:57:23 +0300

    repeat

--- src/main/java/org/lessonWithTeacher/RepeatAfterLesson.java
+++ src/main/java/org/lessonWithTeacher/RepeatAfterLesson.java
@@ -2,7 +2,15 @@ package org.lessonWithTeacher;
 
 public class RepeatAfterLesson {
     public static void main(String[] args) {
+        allElementsInArray();
+    }
 
+    //вывести все элеметны массива
+    public static void allElementsInArray() {
+        int[] array1 = new int[] {1,2,3,4,5};
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
+        }
     }
 
 }
