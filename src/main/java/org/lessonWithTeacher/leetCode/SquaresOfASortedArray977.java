41782a7a091cd94c5ac6a7c33197f1f138bff2dd
Zamira <keldibaevaz@gmail.com>
2026-01-07 22:37:38 +0300

    977

--- src/main/java/org/lessonWithTeacher/leetCode/SquaresOfASortedArray977.java
+++ src/main/java/org/lessonWithTeacher/leetCode/SquaresOfASortedArray977.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SquaresOfASortedArray977 {
     public static void main(String[] args) {
-        int[] array = new int[] {-4,-1,0,3,6};
+        int[] array = new int[] {-4,-1,1,3,6};
         System.out.println(Arrays.toString(sortedSquares(array)));
     }
 
