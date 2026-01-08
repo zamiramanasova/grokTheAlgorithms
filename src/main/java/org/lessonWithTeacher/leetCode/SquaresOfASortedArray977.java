3bbb15bd4230397df4fb90f1668922146cdf83ac
Zamira <keldibaevaz@gmail.com>
2026-01-08 23:05:22 +0300

    977

--- src/main/java/org/lessonWithTeacher/leetCode/SquaresOfASortedArray977.java
+++ src/main/java/org/lessonWithTeacher/leetCode/SquaresOfASortedArray977.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class SquaresOfASortedArray977 {
     public static void main(String[] args) {
         int[] array = new int[] {-4,-1,1,3,6};
-        System.out.println(Arrays.toString(sortedSquares(array)));
+        //System.out.println(Arrays.toString(sortedSquares(array)));
+        System.out.println(Arrays.toString(sortedSquares2(array)));
     }
 
     //мое некорректное решение
@@ -42,5 +43,27 @@ public class SquaresOfASortedArray977 {
         //Space: O(1)
     }
 
+    //корректное решение
+    public static int[] sortedSquares2(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int position = nums.length - 1;
+        int[] result = new int[nums.length];
 
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[position] = leftSquare;
+                left++;
+                position--;
+            } else {
+                result[position] = rightSquare;
+                right--;
+                position--;
+            }
+        }
+        return result;
+    }
 }
