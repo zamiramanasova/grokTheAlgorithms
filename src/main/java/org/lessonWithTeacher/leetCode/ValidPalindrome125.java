58787a21708c212179c3e3dad3f7bdbf48c6cff7
Zamira <keldibaevaz@gmail.com>
2026-01-09 23:54:52 +0300

    344

---
+++ src/main/java/org/lessonWithTeacher/leetCode/ValidPalindrome125.java
@@ -0,0 +1,30 @@
+package org.lessonWithTeacher.leetCode;
+
+public class ValidPalindrome125 {
+    public static void main(String[] args) {
+         String s = "race e car";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+                left++;
+                right--;
+        }
+        return true;
+    }
+}
