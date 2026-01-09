afadf2b449dadb3c618f5099a5b8d5b141443b0f
Zamira <keldibaevaz@gmail.com>
2026-01-09 23:00:56 +0300

    344

--- src/main/java/org/lessonWithTeacher/leetCode/ReverseString344.java
+++ src/main/java/org/lessonWithTeacher/leetCode/ReverseString344.java
@@ -1,7 +1,28 @@
 package org.lessonWithTeacher.leetCode;
 
+import java.util.Arrays;
+
 public class ReverseString344 {
     public static void main(String[] args) {
+        char[] s = new char[] {'h','e','l','l','o'};
+        reverseString(s);
+    }
 
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        char position;
+
+        while (left < right) {
+            position = s[left];
+            s[left] = s[right];
+            s[right] = position;
+
+            right--;
+            left++;
+        }
+        System.out.println(Arrays.toString(s));
+        //Time: O(n)
+        //Space: O(1) доп память постоянная, то есть не создаю новый
     }
 }
