eeba64419a9019058997eb8dcbb77db09867925e
Zamira <keldibaevaz@gmail.com>
2026-01-10 12:56:37 +0300

    27

--- src/main/java/org/lessonWithTeacher/leetCode/RemoveElement27.java
+++ src/main/java/org/lessonWithTeacher/leetCode/RemoveElement27.java
@@ -4,4 +4,17 @@ public class RemoveElement27 {
     public static void main(String[] args) {
 
     }
+
+    public static int[] removeElement(int[] array, int value) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != value) {
+                array[count] = array[i];
+                count++;
+            }
+        }
+
+        return array;
+    }
 }
