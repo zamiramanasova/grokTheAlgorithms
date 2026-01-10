2e1f84c31b1954c93bc6dbd42895a2b4292dea73
Zamira <keldibaevaz@gmail.com>
2026-01-10 13:37:13 +0300

    26

--- src/main/java/org/lessonWithTeacher/leetCode/RemoveDuplicates26.java
+++ src/main/java/org/lessonWithTeacher/leetCode/RemoveDuplicates26.java
@@ -1,4 +1,28 @@
 package org.lessonWithTeacher.leetCode;
 
+import java.util.Arrays;
+
 public class RemoveDuplicates26 {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,2,4};
+        System.out.println(removeDuplicates(array));
+    }
+
+    public static int removeDuplicates(int[] array) {
+        if (array.length == 0) return 0;
+
+        int number = 1;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != array.length - 1) {
+                array[number] = array[number - 1];
+                number++;
+            }
+        }
+
+        return number;
+
+        //Time O(n)
+        //Space O(1)
+    }
 }
