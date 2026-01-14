3b2bfdd3099d14f2a2a95f90f59ed84f13f971c8
Zamira <keldibaevaz@gmail.com>
2026-01-14 23:22:25 +0300

    two pointers

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/SearchInsertPosition35.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/SearchInsertPosition35.java
@@ -3,8 +3,8 @@ package org.lessonWithTeacher.leetCode.binarySearch;
 public class SearchInsertPosition35 {
     public static void main(String[] args) {
         int[] array = new int[] {1,2,3,4,5};
-        int target = 4;
-        System.out.println(search(array, target));
+        int target = 6;
+        System.out.println(searchInsert(array, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
