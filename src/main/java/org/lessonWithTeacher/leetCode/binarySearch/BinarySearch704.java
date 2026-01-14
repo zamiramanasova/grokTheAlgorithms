e55efffec4b9671c1b213832179d281496a6105d
Zamira <keldibaevaz@gmail.com>
2026-01-14 23:02:47 +0300

    two pointers

---
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/BinarySearch704.java
@@ -0,0 +1,28 @@
+package org.lessonWithTeacher.leetCode.binarySearch;
+
+public class BinarySearch704 {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,4,5};
+        int target = 4;
+        System.out.println(search(array, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+}
