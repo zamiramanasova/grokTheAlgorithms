3296a406d1f99ae58e5b4503931a9ff9bd9471b3
Zamira <keldibaevaz@gmail.com>
2026-01-15 22:33:42 +0300

    binary search

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindFirstandLastPositionofElementinSortedArray34.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindFirstandLastPositionofElementinSortedArray34.java
@@ -1,17 +1,30 @@
 package org.lessonWithTeacher.leetCode.binarySearch;
 
+import java.util.Arrays;
+
 public class FindFirstandLastPositionofElementinSortedArray34 {
     public static void main(String[] args) {
-
+        int[] array = new int[] {0,1,1,4,5};
+        int target = 3;
+        System.out.println(Arrays.toString(findFirstAndLast(array, target)));
     }
 
     public static int[] findFirstAndLast(int[] array, int target) {
+
         int left = 0;
         int right = array.length - 1;
-        int number = 0;
+        int number1 = 0;
+        int number2 = 0;
 
         while (left <= right) {
-            int mid =
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                number1 = mid;
+                left = mid + 1;
+                right = mid - 1;
+            }
         }
+        return new int[] {number1, number2};
     }
 }
