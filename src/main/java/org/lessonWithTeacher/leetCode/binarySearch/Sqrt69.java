1203871d11dd83bb9c74e45621ab2ebc306bde22
Zamira <keldibaevaz@gmail.com>
2026-01-16 23:17:33 +0300

    binary search

---
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/Sqrt69.java
@@ -0,0 +1,31 @@
+package org.lessonWithTeacher.leetCode.binarySearch;
+
+public class Sqrt69 {
+    public static void main(String[] args) {
+        int number = 16;
+        System.out.println(sqrt69(number));
+
+    }
+
+    public static int sqrt69(int number) {
+        int left = 0;
+        int right = number;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid; // во избежание переполнения
+            if (square == number) {
+                return mid;
+            } else if (square < number) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return answer;
+    }
+    //Time: O(log n)
+    //Space: O(1)
+}
