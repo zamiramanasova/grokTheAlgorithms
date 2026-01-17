662d4ed2f008572963241976a11d3d3b07f2001d
Zamira <keldibaevaz@gmail.com>
2026-01-17 21:34:38 +0300

    binary search

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/Repeat34.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/Repeat34.java
@@ -6,7 +6,55 @@ public class Repeat34 {
     public static void main(String[] args) {
         int[] array = new int[] {1,2,3,7,7,8};
         int target = 7;
-        System.out.println(Arrays.toString(findFirstAndLast(array, target)));
+        System.out.println(Arrays.toString(findFirstAndLast2(array, target)));
     }
 
+    public static int[] findFirstAndLast2(int[] array, int target) {
+        int first = -1;
+        int last = -1;
+        first = findFirst1(array, target);
+        last = findLast2(array, target);
+
+        return new int[] {first, last};
+    }
+
+    public static int findFirst1(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int first = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return first;
+    }
+
+    public static int findLast2(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        int last = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return last;
+    }
 }
