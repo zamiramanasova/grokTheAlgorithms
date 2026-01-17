fb4bb1f37ef655fbe702ae9795be8f2c22df9bc9
Zamira <keldibaevaz@gmail.com>
2026-01-17 21:24:27 +0300

    binary search

---
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/Repeat34.java
@@ -0,0 +1,12 @@
+package org.lessonWithTeacher.leetCode.binarySearch;
+
+import java.util.Arrays;
+
+public class Repeat34 {
+    public static void main(String[] args) {
+        int[] array = new int[] {1,2,3,7,7,8};
+        int target = 7;
+        System.out.println(Arrays.toString(findFirstAndLast(array, target)));
+    }
+
+}
