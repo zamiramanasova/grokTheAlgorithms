d0bab151a8102c504d907fed581bf7cd699cef2b
Zamira <keldibaevaz@gmail.com>
2026-01-19 21:32:39 +0300

    binary search

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindPeakElement162.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindPeakElement162.java
@@ -2,7 +2,7 @@ package org.lessonWithTeacher.leetCode.binarySearch;
 
 public class FindPeakElement162 {
     public static void main(String[] args) {
-         int[] nums = new  int[] {1,2,1,3,1};
+         int[] nums = new  int[] {1,5,1,1,1};
         System.out.println(peakElement(nums));
     }
 
@@ -10,17 +10,16 @@ public class FindPeakElement162 {
         int left = 0;
         int right = nums.length - 1;
 
-        while (left < right) {
+        while (left < right) {//чтобы не выйти за пределы массива, убираем <= и делаем <
             int mid = left + (right - left) / 2;
 
             if (nums[mid] < nums[mid + 1]) {
                 left = mid + 1;
-                return left;
-            } else {
+            } else {//иначе если nums[mid] > nums[mid + 1]
                 right = mid;
             }
         }
 
-        return right;
+        return left;
     }
 }
