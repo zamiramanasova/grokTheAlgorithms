bcc490d9f61413b09c6289d94103810e25b57cd4
Zamira <keldibaevaz@gmail.com>
2026-01-19 21:12:57 +0300

    binary search

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindPeakElement162.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindPeakElement162.java
@@ -1,4 +1,11 @@
 package org.lessonWithTeacher.leetCode.binarySearch;
 
 public class FindPeakElement162 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int peakElement(int[] nums) {
+
+    }
 }
