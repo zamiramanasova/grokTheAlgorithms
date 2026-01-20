b797dc9ffb6843ad5879377c9bbc42d8e5a9b24b
Zamira <keldibaevaz@gmail.com>
2026-01-20 22:04:50 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/ContainsDuplicate217.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/ContainsDuplicate217.java
@@ -1,8 +1,26 @@
 package org.lessonWithTeacher.leetCode.hashTable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate217 {
     public static void main(String[] args) {
+        int[] nums = new  int[] {1,5,4,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
 
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> setNums = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (setNums.contains(nums[i])) {
+                return true;
+            } else {
+                setNums.add(nums[i]);
+            }
+        }
+        return false;
     }
 
 }
