3a1bff2e66d3b699ed4b546a1da8a787572841e6
Zamira <keldibaevaz@gmail.com>
2026-01-21 22:23:31 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/ValidAnagram242.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/ValidAnagram242.java
@@ -1,4 +1,18 @@
 package org.lessonWithTeacher.leetCode.hashTable;
 
+import java.util.Hashtable;
+
 public class ValidAnagram242 {
+    public static void main(String[] args) {
+        String s = "read";
+        String b = "read";
+        System.out.println(anagram(s, b));
+    }
+    public static boolean anagram(String s, String b) {
+
+        Hashtable<Character, Integer> hashtable = new Hashtable<>();
+
+        for (int i = 0; i < )
+
+    }
 }
