bdf7af94a95be7ee6612bcadabcbab21a85f5093
Zamira <keldibaevaz@gmail.com>
2026-01-25 22:02:04 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/MajorityElements169.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/MajorityElements169.java
@@ -4,14 +4,23 @@ import java.util.HashMap;
 
 public class MajorityElements169 {
     public static void main(String[] args) {
-
+        int[] nums = new int[] {1,2,2,3};
+        System.out.println(majorityElement(nums));
     }
 
     public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        int b = 0;
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-
+             int a = nums[i];
+             hashMap.put(a, hashMap.getOrDefault(a,0) + 1);
+             if (nums[i] > n/2) {
+                 b = nums[i];
+                 return b;
+             }
         }
+        return b;
     }
 }
