52c0322bf2806f702ffb1347c410c9351a0f1f9e
Zamira <keldibaevaz@gmail.com>
2026-01-25 21:52:23 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/MajorityElements169.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/MajorityElements169.java
@@ -1,4 +1,17 @@
 package org.lessonWithTeacher.leetCode.hashTable;
 
+import java.util.HashMap;
+
 public class MajorityElements169 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
 }
