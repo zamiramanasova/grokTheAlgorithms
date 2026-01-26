7b5eb749fd6d6918ba5f4bc1a6db38749f64f2c4
Zamira <keldibaevaz@gmail.com>
2026-01-26 21:30:47 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/IntersectionOfTwoArrays349.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/IntersectionOfTwoArrays349.java
@@ -5,7 +5,9 @@ import java.util.Set;
 
 public class IntersectionOfTwoArrays349 {
     public static void main(String[] args) {
-
+        int[] nums = new int[] {1,2,3};
+        int[] nums1 = new int[] {11,22,3};
+        System.out.println(intersectionOfTwoArrays(nums, nums1));
     }
 
     public static Set<Integer> intersectionOfTwoArrays(int[] nums1, int[] nums2) {
