89240c07bcf0bafe907c1e36b9fe9837fa544d8f
Zamira <keldibaevaz@gmail.com>
2026-01-26 21:32:59 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/IntersectionOfTwoArrays349.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/IntersectionOfTwoArrays349.java
@@ -16,12 +16,12 @@ public class IntersectionOfTwoArrays349 {
 
         for (int i = 0; i < nums1.length; i++) {
             int a = nums1[i];
-            set1.add(a);
+            set1.add(nums1[i]);
         }
 
         for (int i = 0; i < nums2.length; i++) {
             if (set1.contains(nums2[i])) {
-                result.add(i);
+                result.add(nums2[i]);
             }
         }
         return result;
