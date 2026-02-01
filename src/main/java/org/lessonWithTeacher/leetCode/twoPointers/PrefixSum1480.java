e5bf817678c366e9bcb064ee36f859c8f45ed8f0
Zamira <keldibaevaz@gmail.com>
2026-02-01 17:34:01 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/twoPointers/PrefixSum1480.java
+++ src/main/java/org/lessonWithTeacher/leetCode/twoPointers/PrefixSum1480.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
  * Вернём текущую сумму .nums
  *
  * Пример 1:
- *
  * Input: nums = [1,2,3,4]
  * Output: [1,3,6,10]
  * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
