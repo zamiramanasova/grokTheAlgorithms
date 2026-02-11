768c76a8bc1b1de4a8fa15fcbbab3f2a85801fd5
Zamira <keldibaevaz@gmail.com>
2026-02-11 22:56:06 +0300

    hash table

--- src/main/java/org/lessonWithTeacher/leetCode/twoPointers/PrefixSum1480.java
+++ src/main/java/org/lessonWithTeacher/leetCode/twoPointers/PrefixSum1480.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
  */
 public class PrefixSum1480 {
     public static void main(String[] args) {
+
        int[] array = new int[] {1,2,3,4};
        System.out.println(Arrays.toString(runningSum(array)));
     }
