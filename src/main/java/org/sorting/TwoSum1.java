8918cee72a9ac5c99fa637067b1ed9edae9f352c
Zamira <keldibaevaz@gmail.com>
2026-02-15 22:00:27 +0300

    hash table

--- src/main/java/org/sorting/TwoSum1.java
+++ src/main/java/org/sorting/TwoSum1.java
@@ -17,7 +17,6 @@ public class TwoSum1 {
         System.out.println(Arrays.toString(twoSum(nums, target)));
 
     }
-
     public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
