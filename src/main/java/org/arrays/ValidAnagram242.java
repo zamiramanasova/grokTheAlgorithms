27c0496de85a44f7f049f15c077217ea1e1c8d88
Zamira <keldibaevaz@gmail.com>
2026-02-17 21:18:17 +0300

    hash table

--- src/main/java/org/arrays/ValidAnagram242.java
+++ src/main/java/org/arrays/ValidAnagram242.java
@@ -28,11 +28,10 @@ public class ValidAnagram242 {
          if (s.length() != t.length()) {
              return false;
          }
-
          char[] s1 = s.toCharArray();
          char[] t1 = t.toCharArray();
 
-        Arrays.sort(s1);
+         Arrays.sort(s1);
         Arrays.sort(t1);
 
         return Arrays.equals(s1, t1);
