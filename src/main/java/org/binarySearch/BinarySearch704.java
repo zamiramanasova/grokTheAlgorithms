e8d05b91e8f2b4f726ddd1dae1c5c672551095c7
Zamira <keldibaevaz@gmail.com>
2026-02-18 20:35:18 +0300

    hash table

--- src/main/java/org/binarySearch/BinarySearch704.java
+++ src/main/java/org/binarySearch/BinarySearch704.java
@@ -10,7 +10,6 @@ package org.binarySearch;
  * Output: 4
  * Explanation: 9 exists in nums and its index is 4
  * Пример 2:
-
  * Input: nums = [-1,0,3,5,9,12], target = 2
  * Output: -1
  * Explanation: 2 does not exist in nums so return -1
