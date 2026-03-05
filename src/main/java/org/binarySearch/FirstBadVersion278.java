1b19332150b4cc6eb50591a22316b43f88dd6e39
Zamira <keldibaevaz@gmail.com>
2026-03-05 16:43:40 +0300

    hash table

--- src/main/java/org/binarySearch/FirstBadVersion278.java
+++ src/main/java/org/binarySearch/FirstBadVersion278.java
@@ -12,7 +12,7 @@ package org.binarySearch;
  * You are given an API bool isBadVersion(version) which returns whether version is bad.
  * Implement a function to find the first bad version.
  * You should minimize the number of calls to the API.
- *
+
  *
  * Example 1:
  *
