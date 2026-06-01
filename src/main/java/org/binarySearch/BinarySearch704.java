05e763bf0662646c88f7870202c2febc13883fa8
Zamira <keldibaevaz@gmail.com>
2026-06-01 23:35:53 +0300

    hash table 01.06.2026

--- src/main/java/org/binarySearch/BinarySearch704.java
+++ src/main/java/org/binarySearch/BinarySearch704.java
@@ -47,7 +47,7 @@ public class BinarySearch704 {
 
 // Time Complexity O(log n)
 // Space Complexity O(1)
-//Почему память O(1)?
+// Почему память O(1)?
 //Потому что в коде бинарного поиска ты используешь:
 //3 целых числа: low, high, mid
 //одну переменную guess
