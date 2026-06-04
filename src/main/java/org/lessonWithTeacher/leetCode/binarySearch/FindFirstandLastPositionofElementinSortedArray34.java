9e874064e63157f078a3bf2edb526b9938bb67d9
Zamira <keldibaevaz@gmail.com>
2026-06-04 16:40:47 +0300

    hash table 03.06.2026

--- src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindFirstandLastPositionofElementinSortedArray34.java
+++ src/main/java/org/lessonWithTeacher/leetCode/binarySearch/FindFirstandLastPositionofElementinSortedArray34.java
@@ -61,7 +61,7 @@ public class FindFirstandLastPositionofElementinSortedArray34 {
     // Time O(log n)
     // Space O(1)
     // количество переменных постоянно
-    //независимо от того:
+    // независимо от того:
     //массив из 10 элементов
     //или из 10 миллионов
 }
