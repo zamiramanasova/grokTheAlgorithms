7916e2cbe18e98bd79954cce01db613a80277933
Zamira <keldibaevaz@gmail.com>
2026-06-07 14:06:55 +0300

    hash table 06.06.2026

--- src/main/java/org/lessonWithTeacher/leetCode/hashTable/HappyNumber202.java
+++ src/main/java/org/lessonWithTeacher/leetCode/hashTable/HappyNumber202.java
@@ -35,7 +35,7 @@ public class HappyNumber202 {
         return sum;
         // Что делает getNext
         // getNext(19) → 82
-        //getNext(82) → 68
+        // getNext(82) → 68
         //getNext(68) → 100
         //getNext(100) → 1
         //Это просто переходы.
