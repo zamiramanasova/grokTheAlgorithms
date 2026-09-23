417e4e77a4bcb84953c7bdb777ab1940cfd2196b
Zamira <keldibaevaz@gmail.com>
2026-09-23 18:40:23 +0300

    hash table 16.06.2026

--- src/main/java/org/arrays/BestTimeToBuyAndSellStock121.java
+++ src/main/java/org/arrays/BestTimeToBuyAndSellStock121.java
@@ -8,7 +8,7 @@ package org.arrays;
  * этой сделки. Если вы не можете получить прибыль — возвращайтесь.0
  * Пример 1:
  * Input: prices = [7,1,5,3,6,4]
- * Output: 5
+ * Output:  5
  * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
  */
 public class BestTimeToBuyAndSellStock121 {
